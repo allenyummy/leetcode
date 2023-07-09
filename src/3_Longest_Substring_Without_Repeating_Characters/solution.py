8e84c7a3b4d5fa6c19447f24cac8eb8a23c22867
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2023-07-09 18:25:43 +0800

    fix: 3_Longest_Substring_Without_Repeating_Characters

--- src/3_Longest_Substring_Without_Repeating_Characters/solution.py
+++ src/3_Longest_Substring_Without_Repeating_Characters/solution.py
@@ -2,19 +2,35 @@
 
 
 ## TC: O(N^2)
-## SC: O(N)
+## SC: O(1)
 def lengthOfLongestSubstring(s: str) -> int:
     window_size = 0
     substring = ""
     for char in s:
         if char in substring:
             window_size = max(window_size, len(substring))
-            char_index = substring.index(char)
+            char_index = substring.index(char) ## O(N)
             substring = substring[char_index + 1 :]
         substring += char
     window_size = max(window_size, len(substring))
     return window_size
 
 
+## TC: O(N)
+## SC: O(N)
+def lengthOfLongestSubstring2(s: str) -> int:
+    seen = {}  ## key: char, value: last_seen_index
+    l = 0
+    length = 0
+    for r, char in enumerate(s):
+        if char in seen and seen[char] >= l:
+            l = seen[char] + 1
+        else:
+            length = max(length, r - l + 1)
+        seen[char] = r
+    return length
+
+
 if __name__ == "__main__":
     print(lengthOfLongestSubstring("dvdf"))  ## 3
+    print(lengthOfLongestSubstring2("dvdf"))  ## 3
--- src/3_Longest_Substring_Without_Repeating_Characters/solution.ts
+++ src/3_Longest_Substring_Without_Repeating_Characters/solution.ts
@@ -1,6 +1,5 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-
 // TC: O(N^2)
 // SC: O(N)
 function lengthOfLongestSubstring(s: string): number {
@@ -16,4 +15,22 @@ function lengthOfLongestSubstring(s: string): number {
   }
   window_size = Math.max(look_up.length, window_size);
   return window_size;
+}
+
+// TC: O(N)
+// SC: O(N)
+function lengthOfLongestSubstring2(s: string): number {
+  const seen = new Map<string, number>();
+  let l = 0;
+  let length = 0;
+  for (let r = 0; r <= s.length - 1; r++) {
+    const char = s[r];
+    if (seen.has(char) && seen.get(char) >= l) {
+      l = seen.get(char) + 1;
+    } else {
+      length = Math.max(length, r - l + 1);
+    }
+    seen.set(char, r);
+  }
+  return length;
 }
